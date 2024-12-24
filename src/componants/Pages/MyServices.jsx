e27b6eea00c48e75f006b3c46333eec9dfb7d482
Sonia Binty <sonianazninbinty@gmail.com>
2024-12-25 04:54:36 +0600

    added deleted functionality and modal for delete

--- src/componants/Pages/MyServices.jsx
+++ src/componants/Pages/MyServices.jsx
@@ -3,11 +3,17 @@ import AuthContext from '../provider/AuthContext';
 import axios from 'axios';
 import { FaEdit } from 'react-icons/fa';
 import { MdDeleteForever } from 'react-icons/md';
+import Swal from 'sweetalert2';
+import toast from 'react-hot-toast';
 
 const MyServices = () => {
 
   const{user}=useContext(AuthContext)
   const [servs ,setServices ] = useState([])
+
+
+
+
 
   useEffect(() => {
   
@@ -22,6 +28,48 @@ const MyServices = () => {
     
   
   }, [user.email]);
+
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        axios
+        .delete(`http://localhost:5000/service/${id}`)
+       
+         
+          .then((data) => {
+            if (data.data.deletedCount > 0) {
+                
+              Swal.fire({
+                title: "Good job!",
+                text: "You movie has benn delete from favorite",
+                icon: "success",
+
+              
+              });
+              const remaining = servs.filter(s => s._id !== id)
+              setServices(remaining)
+   
+            } else {
+              toast.error("Failed to delete the movie");
+            }
+          })
+          .catch(() => toast.error("An error occurred while deleting the movie"));
+      }
+    });
+  };
+                    
+
+  
   return (
     <div className='max-w-7xl mx-auto my-12'>
     
@@ -50,7 +98,9 @@ const MyServices = () => {
         <td className='text-md'> ${serv.price}</td>
         <td className='flex items-center gap-4 text-center'>
         <FaEdit className='text-2xl text-green-900'/>
-        <MdDeleteForever className='text-2xl text-red-900' />
+      <button  onClick={() => handleDelete(serv._id)}>
+      <MdDeleteForever className='text-2xl text-red-900' />
+      </button>
 
         </td>
       </tr>
