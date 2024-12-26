99510cb3fd1d53a8402e4c73252e39f8369d622b
Sonia Binty <sonianazninbinty@gmail.com>
2024-12-26 06:58:35 +0600

    added countup section

--- src/componants/Shared/Home.jsx
+++ src/componants/Shared/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Count from '../componant/Count';
+
 
 
 const Home = () => {
   return (
     <div>
+      <Count></Count>
      
       
     </div>
---
+++ src/componants/componant/Count.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CountUp from 'react-countup';
+import { GoCodeReview } from 'react-icons/go';
+import { LuUserRoundPlus } from 'react-icons/lu';
+import { MdMiscellaneousServices } from 'react-icons/md';
+
+const Count = () => {
+ const [reviews,  setReviews] = useState([]);
+ const [users,  setUsers] = useState([]);
+ const [services,  setServices] = useState([]);
+ useEffect(() => {
+
+   
+    axios
+      .get('http://localhost:5000/review')
+      .then((response) => {
+        setReviews(response.data);
+      })
+      axios
+      .get('http://localhost:5000/users')
+      .then((response) => {
+        setUsers(response.data);
+      })
+
+      axios
+      .get('http://localhost:5000/service')
+      .then((response) => {
+        setServices(response.data);
+      })
+ 
+}, []);
+
+
+  return (
+    <div className='md:flex justify-between max-w-7xl mx-auto bg-green-900 text-white  px-[160px] py-10 my-12 rounded-lg'>
+
+<div className='text-center  space-y-2'>
+
+<LuUserRoundPlus className='text-3xl font-bold mx-auto mb-3 '/>
+
+<CountUp className='text-4xl font-bold' end={users.length} duration={5} />
+<h1 className='text-3xl font-bold'>Users</h1>
+
+</div>
+
+<div className='text-center space-y-2'>
+
+<MdMiscellaneousServices className='text-3xl font-bold mx-auto mb-3 '/>
+
+<CountUp className='text-4xl font-bold' end={services.length} duration={5} />
+<h1 className='text-3xl font-bold'>Services</h1>
+
+</div>
+
+
+
+    <div className='text-center space-y-2'>
+
+    <GoCodeReview className='text-3xl font-bold mx-auto mb-3 '/>
+   
+    <CountUp className='text-4xl font-bold' end={ reviews.length} duration={5} />
+ <h1 className='text-3xl font-bold'>Reviews</h1>
+
+    </div>
+
+    
+
+   
+    
+  </div>
+  );
+};
+
+export default Count;
