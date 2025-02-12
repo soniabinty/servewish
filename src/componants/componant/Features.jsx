4a08c8b08c2aa729019fbcd74b47aa76011ab07b
Sonia Binty <sonianazninbinty@gmail.com>
2025-02-12 06:03:31 +0600

    added updating of feature section

--- src/componants/componant/Features.jsx
+++ src/componants/componant/Features.jsx
@@ -25,31 +25,32 @@ const Features = () => {
   return (
    <div>
     
-    <h1 className='text-center md:text-3xl text-xl font-bold text-green-900'>Feature Services</h1>
+    <h1 className='text-center md:text-4xl text-xl font-bold text-[#fb110d]'>Feature Services</h1>
       <p className='text-center '>We are Always with you. Get our Feature collection.</p>
-     <div className='md:grid grid-cols-3 gap-10 p-3n md:px-6'>
+     <div className='md:grid grid-cols-4 gap-2 p-3n md:px-12'>
 
 {
   features.map((feature =>(
-    <div class="flex flex-col bg-white shadow-sm border border-slate-200 rounded-lg my-6 ">
-    <div class="m-2.5 overflow-hidden rounded-md h-80 flex justify-center items-center">
-      <img class="w-full h-full object-cover" src={feature.image} alt="profile-picture" />
+    <div class="flex flex-col bg-white shadow-sm border border-slate-200 py-3 rounded-lg my-6 ">
+    <div class=" overflow-hidden   flex justify-center items-center">
+      <img class="h-[200px] w-full mt-2 object-cover" src={feature.image} alt="profile-picture" />
     </div>
-    <div class="p-6 text-center">
+    <div class="text-start pl-3 mt-2">
+        <p
+        class="text-md font-semibold text-slate-500 uppercase">
+       ${feature.price}
+      </p>
       <h4 class="mb-1 text-xl font-semibold text-slate-800">
         {feature.title}
       </h4>
-      <p
-        class="text-sm font-semibold text-slate-500 uppercase">
-       ${feature.price}
-      </p>
-      <p class="text-base text-slate-600 mt-4 font-light ">
+    
+      <p class="text-base text-slate-600 mt-2 font-light ">
       {feature.description?.substring(0, 100)}.....
       </p>
     </div>
-    <div class="flex justify-center p-6 pt-2 gap-7">
+    <div class="flex justify-start px-4 py-2 ">
    <Link to={`/services/${feature._id}`}>
-   <button class="min-w-32 bg-green-900  rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+   <button class="min-w-32 bg-[#fb110d]  rounded-md bg-slate-800 p-2  border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
        See Details
       </button></Link>
     </div>
